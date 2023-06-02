b89aeb181658994e9a116430c0a594faff3ef823
Mythage <so.nijdam@gmail.com>
2023-06-02 23:13:19 +0200

    einde dag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom";
 import LandingPage from './pages/LandingPage/LandingPage';
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
       <>
     <Routes>
       <Route path="/" element={<LandingPage/>} />
+      <Route Path="*" element={<NotFound/>} />
     </Routes>
         </>
   );
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+    return (
+        <>
+            <h2>OEPS.... DEZE PAGINA IS NIET GEVONDEN</h2>
+        </>
+    );
+}
+
+export default NotFound;
