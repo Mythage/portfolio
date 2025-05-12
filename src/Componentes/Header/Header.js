2ec1d1f89d8c142526e68deef572e56ee47d2b2d
sonij <so.nijdam@gmail.com>
2025-05-12 16:50:19 +0200

    Started Working on the Header.js
    Signed-off-by: sonij <so.nijdam@gmail.com>

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.css';
 
 function Header() {
-    return (
+    const [count, setCount] = useState(0); // State voor de teller
 
+    const incrementCount = () => {
+        setCount(count + 1); // Functie om de teller te verhogen
+    };
+
+    return (
         <div>
             <section>
                 <h2>31</h2>
-                <h3>Level</h3>
+                <h4>Level</h4>
             </section>
             <section>
-                <button>Play</button>
-                <h2>number</h2>
-                <h3>Coins awarded</h3>
+                <button onClick={incrementCount}>+</button>{/*button om de tellel te verhogen*/}
+                <h2>{count}</h2> {/* De actuele waarde van de teller */}
+                <h4>Coins awarded</h4>
             </section>
         </div>
 
-    )
+    );
 }
 
 export default Header;
